e4dc5063d4f0b5f260201805638c059c06b94255
Sam Wilson <tecywiz121@hotmail.com>
2013-02-24 15:54:56 -0500

    Make parser kind of work

--- godfrey.py
+++ godfrey.py
@@ -2,42 +2,40 @@
 # godfrey.py is the point of entry, point of exit, and controller of the Godfrey program.
 
 import sys
-from . import *
 import modules
+from parser import Parser
 
 
-def godfrey(command):
+def godfrey(text):
     '''Given the command, execute the function with the appropriate modules.'''
     # get a list of modules
     all_modules = modules.all_modules()
 
     # create a parser with the modules and parse the command
     parser = Parser(all_modules)
-    command = parser.parse(command)
 
     # Use the command object to decide what module(s) qualify to run it.
-    qualifying_modules = []
-    for module in all_modules:
-        if module.can_handle(command):
-            qualifying_modules.append(module)
+    for command in parser.parse(text):
+        qualifying_modules = []
+        for module in all_modules:
+            if module.can_handle(command):
+                qualifying_modules.append(module)
 
-    # Pass the command to each qualifying module and save the response.
-    responses = []
-    for module in qualifying_modules:
-        responses.append(module.run(command))
+        # Pass the command to each qualifying module and save the response.
+        responses = []
+        for module in qualifying_modules:
+            responses.append(module.run(command))
 
-    # print the output of each response and exit with the proper code
-    for response in responses:
-        print response.message
-        if response.status == response.SUCCESS:
-            exit(0)
-        elif responsoe.status == response.OBTUSE:
-            exit(3)
-        elif not response.status == response.FAILED:
-            exit(2)
-        else:
-            print "Godfrey made an error. Godfrey was an error."
-            exit(1)
+        # print the output of each response and exit with the proper code
+        for response in responses:
+            print response.message
+            if response.status == response.OBTUSE:
+                exit(3)
+            elif not response.status == response.FAILED:
+                exit(2)
+            else:
+                print "Godfrey made an error. Godfrey was an error."
+                exit(1)
 
 # Join the command into one string and pass to the main function
 godfrey(' '.join(sys.argv[1:]))
--- parser.py
+++ parser.py
@@ -1,18 +1,63 @@
-
+from nltk import pos_tag, word_tokenize, Tree
+from nltk.parse.malt import MaltParser
+from collections import defaultdict
+from .command import Command
 
 class Parser(object):
     '''Write something here.'''
-    nouns = {}
-    verbs = {}
+    nouns = defaultdict(set)
+    verbs = defaultdict(set)
 
-    def __init__(self, all_modules):
+    def __init__(self, all_modules, malt_working_dir=None, malt_mco=None):
         '''...'''
+
+        if not malt_working_dir:
+            import os
+            malt_working_dir = os.environ['MALTPARSERHOME']
+
+        if not malt_mco:
+            malt_mco = 'engmalt.linear-1.7'
+
+        self._parser = MaltParser(working_dir=malt_working_dir, mco=malt_mco)
+
         for module in all_modules:
-            for noun in module.nouns():
-                self.nouns[noun] = module
-            for verb in module.verbs():
-                self.verbs[verb] = module
+            for noun in module.nouns:
+                self.nouns[noun].add(module)
+            for verb in module.verbs:
+                self.verbs[verb].add(module)
+
+    def _is_verb(self, word):
+        return word in self.verbs
+
+    def _is_noun(self, word):
+        return word in self.nouns
+
+    def _check_command(self, noun, verb):
+        if not noun or not verb:
+            return False
+        return self.nouns[noun] & self.verbs[verb]
 
     def parse(self, command):
         '''...'''
-        pass
+        words = word_tokenize(command)
+        pos = pos_tag(words)
+        graph = self._parser.tagged_parse(pos)
+        tree = graph.tree()
+
+        c_noun = None
+        c_verb = None
+
+        for subtree in tree.subtrees():
+            words = [x for x in subtree if not isinstance(x, Tree)] + [subtree.node]
+            for x in words:
+                # TODO: Use the tagged sentence so that we can resolve this better
+                if self._is_verb(x):
+                    c_verb = x
+                elif self._is_noun(x):
+                    c_noun = x
+                command_modules = self._check_command(c_noun, c_verb)
+                if command_modules:
+                    yield Command(command, c_noun, c_verb, [])
+
+        import code
+        code.interact(local=locals())
