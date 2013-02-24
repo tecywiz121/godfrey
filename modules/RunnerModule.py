826c97d57770ff88df3f6d086ee76e2090285b37
Sam Wilson <tecywiz121@hotmail.com>
2013-02-24 16:48:09 -0500

    Fix file paths

--- modules/RunnerModule.py
+++ modules/RunnerModule.py
@@ -1,25 +1,30 @@
 import subprocess
 import module
 
-class RunFile(module.Module):
+class RunnerModule(module.Module):
     '''modulePath: path to the module, arg: parameters to pass'''
     def winRunner(self, modulePath, arg):
 
         print 'Runner.py started'
         print 'Running program:', str(modulePath), 'with arguements:', str(arg)
         subprocess.call(["C:\Python27\python.exe", modulePath, arg])
-	
+
     def runner(self, modulePath, arg):
 
         print 'Runner.py started'
         print 'Running program:', str(modulePath), 'with arguements:', str(arg)
         subprocess.call([modulePath, arg])
 
+    @property
     def verbs(self):
         return ['run', 'open']
 
+    @property
     def nouns(self):
         return ['file']
 
     def can_handle(self, command):
-        return (command.noun in nouns() and verbs.verb in verbs())
+        return (command.noun in self.nouns and command.verb in self.verbs)
+
+    def run(self, command):
+        subprocess.call([command.noun])
--- modules/Script Runner/runModRunner.py
+++
@@ -1,4 +0,0 @@
-import modRunner
-
-run = modRunner.RunFile()
-run.winRunner("C:\hello.py", "what a wonderfull world")
