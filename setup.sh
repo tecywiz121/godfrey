93574562656b5dd30a6468516e971d35b80fe6eb
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-03 11:34:52 -0500

    Stub for setup script.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# wget http://maltparser.org/dist/maltparser-1.7.2.tar.gz&
+# WGET_MALP_PID=$!
+# wget http://www.maltparser.org/mco/english_parser/engmalt.linear-1.7.mco&
+# WGET_GRAMMAR_PID=$!
+# 
+# 
+# wait $WGET_MALT_PID
+# tar xvf maltparser-1.7.2.tar.gz
+# mv maltparser-1.7.2 maltparse
+# mv maltparse/maltparser-1.7.2.jar maltparse/malt.jar
+# 
+# wait $WGET_GRAMMAR_PID
+# mv engmalt.linear-1.7.mco maltparse/engmalt.linear-1.2.mco
+
+virtualenv --distribute venv
+source venv/bin/activate
+
+pip install nltk&
+PIP_INSTALL_PID=$!
